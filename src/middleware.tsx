12b15c4ece238941370624d001bda4ebe26f1cb5
mauryavinay1407 <vinaymaurya1407@gmail.com>
2024-09-12 01:23:02 +0530

    Fix:middleware issue

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,12 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextRequest,NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/',
+]);
+
+export default clerkMiddleware((auth, request) => {
+  const { userId } = auth();
+  const currentUrl = new URL(request.url);
+
+  if(!userId){
+    if(!isPublicRoute(request))
+      return NextResponse.redirect(new URL('/sign-in',request.url));
+  }
+
+  return NextResponse.next();
+});
+
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next|_oauth).*)', '/', '/(api|trpc)(.*)'],
 };
